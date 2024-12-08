e98673859cbbe946cba2eebcaf243b3229c18b01
Harsh <harshjha872@gmail.com>
2024-12-08 15:27:09 +0530

    Added frontend structure

---
+++ app/components/DateTopbar/DateTopbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DateTopbar = () => {
+  return (
+    <div>
+      Date top bar
+    </div>
+  )
+}
+
+export default DateTopbar
---
+++ app/components/SingleTask/SingleTask.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleTask = () => {
+  return (
+    <div>
+      single task
+    </div>
+  )
+}
+
+export default SingleTask
